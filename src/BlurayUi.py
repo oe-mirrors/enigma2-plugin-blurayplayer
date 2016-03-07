f084bacc79470b409e1e8c950de99f20e9bbc487
Taapat <taapat@gmail.com>
2016-03-08 00:07:39 +0200

    Add BlurayAudioSelection
    This allow use track language codes from titles.
    In enigma them can not be determined.

--- src/BlurayUi.py
+++ src/BlurayUi.py
@@ -3,11 +3,14 @@ import os
 from enigma import ePicLoad, eServiceReference, eTimer, getDesktop
 from Components.ActionMap import ActionMap
 from Components.AVSwitch import AVSwitch
+from Components.config import config, getConfigListEntry, \
+	ConfigNothing, ConfigSelection, ConfigOnOff
 from Components.Console import Console
 from Components.Label import Label
 from Components.Pixmap import Pixmap
 from Components.Sources.List import List
 from Components.Sources.StaticText import StaticText
+from Screens.AudioSelection import AudioSelection
 from Screens.InfoBar import InfoBar, MoviePlayer
 from Screens.MessageBox import MessageBox
 from Screens.Screen import Screen
@@ -17,11 +20,132 @@ from . import _
 import blurayinfo
 
 
+class BlurayAudioSelection(AudioSelection):
+	def __init__(self, session, infobar, page, languages, codecs):
+		AudioSelection.__init__(self, session, infobar, page)
+		self.skinName = 'AudioSelection'
+		self.languages = languages
+		self.codecs = codecs
+
+	def fillList(self, arg=None):
+		from Components.SystemInfo import SystemInfo
+		from Tools.ISO639 import LanguageCodes
+
+		streams = []
+		conflist = []
+		selectedidx = 0
+
+		self['key_blue'].setBoolean(False)
+
+		if self.settings.menupage.getValue() == 'audio':
+			self.setTitle(_('Select audio track'))
+			service = self.session.nav.getCurrentService()
+			self.audioTracks = audio = service and service.audioTracks()
+			n = audio and audio.getNumberOfTracks() or 0
+			if SystemInfo['CanDownmixAC3']:
+				self.settings.downmix = ConfigOnOff(default=config.av.downmix_ac3.value)
+				self.settings.downmix.addNotifier(self.changeAC3Downmix,
+						initial_call=False)
+				conflist.append(getConfigListEntry(_('Multi channel downmix'),
+						self.settings.downmix))
+				self['key_red'].setBoolean(True)
+
+			if n > 0:
+				self.audioChannel = service.audioChannel()
+				if self.audioChannel:
+					choicelist = [('0', _('left')), ('1', _('stereo')), ('2', _('right'))]
+					self.settings.channelmode = ConfigSelection(choices=choicelist,
+							default=str(self.audioChannel.getCurrentChannel()))
+					self.settings.channelmode.addNotifier(self.changeMode, initial_call=False)
+					conflist.append(getConfigListEntry(_('Channel'), self.settings.channelmode))
+					self['key_green'].setBoolean(True)
+				else:
+					conflist.append(('',))
+					self['key_green'].setBoolean(False)
+				selectedAudio = self.audioTracks.getCurrentTrack()
+				li = 0
+				self.codecs.append('END')
+				for x in range(n):
+					number = str(x + 1)
+					i = audio.getTrackInfo(x)
+					languages = i.getLanguage().split('/')
+					description = i.getDescription() or ''
+					selected = ''
+					language = ''
+
+					if not languages[0] and description:
+						while self.codecs[li] != description and self.codecs[li] != 'END':
+							li += 1
+						if self.codecs[li] == 'END':
+							li = 0
+						else:
+							languages[0] = self.languages[li]
+							li += 1
+
+					if selectedAudio == x:
+						selected = 'X'
+						selectedidx = x
+
+					cnt = 0
+					for lang in languages:
+						if cnt:
+							language += ' / '
+						if lang in LanguageCodes:
+							language += _(LanguageCodes[lang][0])
+						elif lang == 'und':
+							''
+						else:
+							language += lang
+						cnt += 1
+
+					streams.append((x, '', number, description, language, selected))
+
+			else:
+				streams = []
+				conflist.append(('',))
+				self['key_green'].setBoolean(False)
+
+			self['key_yellow'].setBoolean(False)
+			conflist.append(('',))
+
+			from Components.PluginComponent import plugins
+			from Plugins.Plugin import PluginDescriptor
+
+			if hasattr(self.infobar, 'runPlugin'):
+				class PluginCaller:
+					def __init__(self, fnc, *args):
+						self.fnc = fnc
+						self.args = args
+
+					def __call__(self, *args, **kwargs):
+						self.fnc(*self.args)
+
+				self.Plugins = [(p.name, PluginCaller(self.infobar.runPlugin, p))
+						for p in plugins.getPlugins(where=PluginDescriptor.WHERE_AUDIOMENU)]
+
+				if self.Plugins:
+					self['key_blue'].setBoolean(True)
+					if len(self.Plugins) > 1:
+						conflist.append(getConfigListEntry(_('Audio plugins'), ConfigNothing()))
+						self.plugincallfunc = [(x[0], x[1]) for x in self.Plugins]
+					else:
+						conflist.append(getConfigListEntry(self.Plugins[0][0], ConfigNothing()))
+						self.plugincallfunc = self.Plugins[0][1]
+
+		self['config'].list = conflist
+		self['config'].l.setList(conflist)
+
+		self['streams'].list = streams
+		self['streams'].setIndex(selectedidx)
+
+
 class BlurayPlayer(MoviePlayer):
-	def __init__(self, session, service):
+	def __init__(self, session, service, languages, codecs):
 		MoviePlayer.__init__(self, session, service)
 		self.skinName = 'MoviePlayer'
 		self.servicelist = InfoBar.instance and InfoBar.instance.servicelist
+		self.languages = languages
+		self.codecs = codecs
 
 	def handleLeave(self, how):
 		if how == 'ask':
@@ -36,6 +160,10 @@ class BlurayPlayer(MoviePlayer):
 
 	def showMovies(self):
 		pass
+
+	def audioSelection(self):
+		self.session.open(BlurayAudioSelection, infobar=self, page='audio',
+				languages=self.languages, codecs=self.codecs)
 
 
 class BlurayMain(Screen):
@@ -117,14 +245,16 @@ class BlurayMain(Screen):
 		x = 1
 		try:
 			for title in blurayinfo.getTitles(self.res):
-				playfile = os.path.join(self.res, 'BDMV/STREAM/', title[1] + '.m2ts')
 				title_entry = _('%d. Duration %d:%02d minutes') % \
 						(x, title[0] / (45000 * 60), (title[0] / 45000) % 60)
-				content.append((title_entry, playfile))
+				playfile = os.path.join(self.res, 'BDMV/STREAM/', title[1] + '.m2ts')
+				languages = title[2][1:].split('/')
+				codecs = title[3][1:].split('/')
+				content.append((title_entry, playfile, languages, codecs))
 				x += 1
 		except Exception as e:
 			print '[BlurayPlayer] blurayinfo.getTitles:', e
-			content.append((_('Error in reading titles...'), None))
+			content.append((_('Error in reading titles...'), None, None))
 		self['list'].setList(content)
 		self.onLayoutFinish.append(self.LayoutFinish)
 
@@ -180,7 +310,8 @@ class BlurayMain(Screen):
 			ref = eServiceReference(3, 0, current[1])
 			ref.setName('%s - %s' % (self.name, current[1][-10:]))
 			self.Timer.start(20000, True)
-			self.session.openWithCallback(self.MoviePlayerCallback, BlurayPlayer, ref)
+			self.session.openWithCallback(self.MoviePlayerCallback,
+					BlurayPlayer, service=ref, languages=current[2], codecs=current[3])
 
 	def enableCloseScreen(self):
 		self.closeScreen = True
